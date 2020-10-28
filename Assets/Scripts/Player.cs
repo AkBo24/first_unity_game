ffdfa90bd863f0e3a401a91a0ac78a61efbd2a90
Akshay Bodla <akshaybodla@Akshays-MacBook-Pro.local>
2020-10-28 08:23:50 -0700

    My first unity game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    //[SerializeField] private "exposes" the variable to the unity UI but can not be accessed by other scripts
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
+    private bool jumpKeyPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // Make player jump when space is pressed
+        if( Input.GetKeyDown(KeyCode.Space) )
+            jumpKeyPressed = true;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    //Called once every physics update
+    private void FixedUpdate() {
+        
+        rigidbodyComponent.velocity = new Vector3(horizontalInput,rigidbodyComponent.velocity.y,0); 
+
+        if(Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+            return;
+
+
+        if( jumpKeyPressed ) {
+           rigidbodyComponent.AddForce(Vector3.up*4, ForceMode.VelocityChange);
+            jumpKeyPressed = false;
+        }
+
+    }
+
+}
